4873868fc63cfb66be0b919c153896037bd2c4aa
kern yoo <trustyoo86@gmail.com>
2020-10-17 01:03:20 +0900

    feat(modify-textinput): Modify textinput component
    * Modify textinput component

---
+++ src/components/TextForm/TextForm.spec.tsx
@@ -0,0 +1,7 @@
+import { render } from '@testing-library/react';
+
+import TextForm from '../TextForm';
+
+describe('[TextForm] 테스트', () => {
+  test('', () => {});
+});
---
+++ src/components/index.tsx
@@ -0,0 +1,9 @@
+import TextForm from './TextForm';
+import SectionHeader from './SectionHeader';
+import Layout from './Layout';
+
+export {
+  TextForm,
+  SectionHeader,
+  Layout,
+};
